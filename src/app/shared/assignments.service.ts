6aaa838d31d8beb9c6f66b3260d71ca47a12d6f4
flo83440 <aimeflorian1@gmail.com>
2024-01-28 16:02:46 +0100

    url back online

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -18,8 +18,8 @@ export class AssignmentService{
     hasNextPage: any;
     nextPage: any;
 
-    url = "http://localhost:8010/api";
-    // url = "https://assignment-back-c0dfe7c8382c.herokuapp.com/api";
+    // url = "http://localhost:8010/api";
+    url = "https://angularflorianthibaultback.onrender.com//api";
 
     assignments: Assignment[] = [];
     assignmentsMap: Map<number, Assignment> = new Map<number, Assignment>();
